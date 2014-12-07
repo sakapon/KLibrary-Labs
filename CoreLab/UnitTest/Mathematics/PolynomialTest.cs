065064e8e39701694fa93b20bbd0aeb7ad0b3190
Keiho Sakapon <git@saka-pon.net>
2014-12-07 23:00:40 +0900

    Add test

--- CoreLab/UnitTest/Mathematics/PolynomialTest.cs
+++ CoreLab/UnitTest/Mathematics/PolynomialTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
+using System.Windows;
 
 namespace UnitTest.Mathematics
 {
@@ -28,6 +29,40 @@ namespace UnitTest.Mathematics
             CollectionAssert.AreEqual(new[] { -2.0, 0.5 }, ((x + 2) * (2 * x - 1)).SolveQuadraticEquation());
             CollectionAssert.AreEqual(new[] { -0.618, 1.618 }, (x2 - x - 1).SolveQuadraticEquation().Select(d => Math.Round(d, 3, MidpointRounding.AwayFromZero)).ToArray());
             CollectionAssert.AreEqual(new[] { -1.618, 0.618 }, (x2 + x - 1).SolveQuadraticEquation().Select(d => Math.Round(d, 3, MidpointRounding.AwayFromZero)).ToArray());
+        }
+
+        [TestMethod]
+        public void IntersectionTest()
+        {
+            // The intersection of line y = x - 1 and line y = -2x + 5.
+            var l1 = x - 1;
+            var l2 = -2 * x + 5;
+            var p_x = (l1 - l2).SolveLinearEquation();
+            var p_y = l1.Substitute(p_x);
+            Assert.AreEqual(new Point(2, 1), new Point(p_x, p_y));
+        }
+
+        [TestMethod]
+        public void PointsOnLineTest()
+        {
+            var p1 = new Point(0, -300);
+            var p2 = new Point(1800, 300);
+            var y_to_x = GetFunc_y_to_x(p1, p2);
+            Assert.AreEqual(900.0, y_to_x(0));
+            Assert.AreEqual(600.0, y_to_x(-100));
+        }
+
+        // P1, P2 を通る直線上で、指定された y 座標に対応する x 座標を求めるための関数。
+        static Func<double, double> GetFunc_y_to_x(Point p1, Point p2)
+        {
+            // P1 (x1, y1) および P2 (x2, y2) を通る直線の方程式:
+            // (x - x1) (y2 - y1) - (x2 - x1) (y - y1) = 0
+            return y => ((x - p1.X) * (p2.Y - p1.Y) - (p2.X - p1.X) * (y - p1.Y)).SolveLinearEquation();
+        }
+
+        static Func<double, double> GetFunc_y_to_x_Old(Point p1, Point p2)
+        {
+            return y => (p2.X - p1.X) * (y - p1.Y) / (p2.Y - p1.Y) + p1.X;
         }
     }
 }
