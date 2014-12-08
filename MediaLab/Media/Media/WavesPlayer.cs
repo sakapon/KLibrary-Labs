8c8e6257a5e046580057ec4eaa71d44e808b1795
Keiho Sakapon <git@saka-pon.net>
2014-12-08 18:14:27 +0900

    Add WavesPlayer class

--- MediaLab/Media/Media/WavesPlayer.cs
+++ MediaLab/Media/Media/WavesPlayer.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Media
+namespace KLibrary.Lab.Media
 {
-    public class Class1
+    public class WavesPlayer
     {
     }
 }
---
+++ MediaLab/UnitTest/Media/WavesPlayerTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest.Media
+{
+    [TestClass]
+    public class WavesPlayerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
