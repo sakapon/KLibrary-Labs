4b82de920ad07e2e8f2875a882243aff1583553d
Keiho Sakapon <git@saka-pon.net>
2014-12-09 01:21:16 +0900

    Add test

--- MediaLab/UnitTest/Media/WavesPlayerTest.cs
+++ MediaLab/UnitTest/Media/WavesPlayerTest.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using KLibrary.Lab.Media;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace UnitTest.Media
 {
     [TestClass]
     public class WavesPlayerTest
     {
+        const string Empty = "Empty";
+        const string Success = "Success";
+        const string Fail = "Fail";
+
         [TestMethod]
-        public void TestMethod1()
+        public void Play()
         {
+            var sounds = new[] { Empty, Success, Fail }
+                .ToDictionary(n => n, n => string.Format(@"Sounds\{0}.wav", n));
+
+            using (var player = new WavesPlayer(sounds))
+            {
+                player.Play(Empty);
+                Thread.Sleep(200);
+                player.Play(Success);
+                Thread.Sleep(1200);
+                player.Play(Fail);
+                Thread.Sleep(1200);
+            }
         }
     }
 }
