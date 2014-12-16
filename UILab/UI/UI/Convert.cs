43059e06786dc918bda9aa32870d7816ba312b0a
Keiho Sakapon <github@saka-pon.net>
2014-12-16 14:48:02 +0900

    Add ScreenHelper class

---
+++ UILab/UI/UI/Convert.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace KLibrary.Labs.UI
+{
+    public static class Convert
+    {
+        public static Int32Rect ToInt32Rect(this System.Drawing.Rectangle r)
+        {
+            return new Int32Rect(r.X, r.Y, r.Width, r.Height);
+        }
+    }
+}
---
+++ UILab/UI/UI/ScreenHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace KLibrary.Labs.UI
+{
+    public static class ScreenHelper
+    {
+        public static Int32Rect AllScreensBounds
+        {
+            get { return _AllScreensBounds.Value; }
+        }
+
+        static readonly Lazy<Int32Rect> _AllScreensBounds = new Lazy<Int32Rect>(() => SystemInformation.VirtualScreen.ToInt32Rect());
+
+        public static Int32Rect PrimaryScreenBounds
+        {
+            get { return _PrimaryScreenBounds.Value; }
+        }
+
+        static readonly Lazy<Int32Rect> _PrimaryScreenBounds = new Lazy<Int32Rect>(() => Screen.PrimaryScreen.Bounds.ToInt32Rect());
+
+        public static Point GetLeftTop(this Int32Rect rect)
+        {
+            return new Point(rect.X, rect.Y);
+        }
+
+        public static Point GetLeftBottom(this Int32Rect rect)
+        {
+            return new Point(rect.X, rect.Y + rect.Height - 1);
+        }
+
+        public static Point GetRightTop(this Int32Rect rect)
+        {
+            return new Point(rect.X + rect.Width - 1, rect.Y);
+        }
+
+        public static Point GetRightBottom(this Int32Rect rect)
+        {
+            return new Point(rect.X + rect.Width - 1, rect.Y + rect.Height - 1);
+        }
+    }
+}
