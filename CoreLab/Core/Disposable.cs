88f08fc317c59b4db19312f63bf1d9bd290f4334
Keiho Sakapon <git@saka-pon.net>
2014-12-18 00:09:22 +0900

    Add Disposable class

---
+++ CoreLab/Core/Disposable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace KLibrary.Labs
+{
+    public static class Disposable
+    {
+        public static IDisposable FromAction(Action action)
+        {
+            return new DisposableAction(action, false);
+        }
+
+        public static IDisposable FromAction(Action action, bool onFinalizing)
+        {
+            return new DisposableAction(action, onFinalizing);
+        }
+    }
+
+    class DisposableAction : IDisposable
+    {
+        Action _action;
+        bool _onFinalizing;
+        bool _isDisposed;
+
+        public DisposableAction(Action action, bool onFinalizing)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+            _action = action;
+            _onFinalizing = onFinalizing;
+        }
+
+        ~DisposableAction()
+        {
+            Dispose(false);
+        }
+
+        void IDisposable.Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_onFinalizing && !disposing) return;
+            if (_isDisposed) return;
+            _isDisposed = true;
+
+            try
+            {
+                _action();
+            }
+            catch (Exception) { }
+        }
+    }
+}
---
+++ CoreLab/UnitTest/DisposableTest.cs
@@ -0,0 +1,21 @@
+﻿using KLibrary.Labs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class DisposableTest
+    {
+        [TestMethod]
+        public void FromAction()
+        {
+            var count = 0;
+            using (var action = Disposable.FromAction(() => count++))
+            {
+                Assert.AreEqual(0, count);
+            }
+            Assert.AreEqual(1, count);
+        }
+    }
+}
