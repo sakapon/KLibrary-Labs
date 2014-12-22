44e550c0ce32898594e9d04e32c351cd43f8aa50
Keiho Sakapon <git@saka-pon.net>
2014-12-22 20:15:52 +0900

    Add ObservableEvent class

---
+++ CoreLab/Core/Reactive/ObservableEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KLibrary.Labs.Reactive
+{
+    public class ObservableEvent<T> : IObservable<T>
+    {
+        event Action<T> TheEvent;
+
+        public IDisposable AddHandler(Action<T> handler)
+        {
+            if (handler == null) throw new ArgumentNullException("handler");
+
+            TheEvent += handler;
+            return Disposable.FromAction(() => TheEvent -= handler);
+        }
+
+        public void Raise(T arg)
+        {
+            var h = TheEvent;
+            if (h != null) h(arg);
+        }
+
+        IDisposable IObservable<T>.Subscribe(IObserver<T> observer)
+        {
+            if (observer == null) throw new ArgumentNullException("observer");
+
+            return AddHandler(observer.OnNext);
+        }
+    }
+}
