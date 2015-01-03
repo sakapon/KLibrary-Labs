30d2ff539b76489d98f86fd4b0fe90c48280d193
Keiho Sakapon <git@saka-pon.net>
2015-01-03 09:24:53 +0900

    Add Notifier class

---
+++ CoreLab/Core/Reactive/Notifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KLibrary.Labs.Reactive
+{
+    public class Notifier<T> : IObservable<T>
+    {
+        List<IObserver<T>> observers = new List<IObserver<T>>();
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            if (observer == null) throw new ArgumentNullException("observer");
+
+            observers.Add(observer);
+            return Disposable.FromAction(() => observers.Remove(observer));
+        }
+
+        public void NotifyNext(T value)
+        {
+            observers.ForEach(o => o.OnNext(value));
+        }
+
+        public void NotifyError(Exception error)
+        {
+            observers.ForEach(o => o.OnError(error));
+        }
+
+        public void NotifyCompleted()
+        {
+            observers.ForEach(o => o.OnCompleted());
+        }
+    }
+}
