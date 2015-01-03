2066a315b02d545624e716999a850968ce08b543
Keiho Sakapon <git@saka-pon.net>
2015-01-03 16:05:49 +0900

    Add NotifierBase class

--- CoreLab/Core/Reactive/Notifier.cs
+++ CoreLab/Core/Reactive/Notifier.cs
@@ -1,34 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace KLibrary.Labs.Reactive
 {
-    public class Notifier<T> : IObservable<T>
+    public class Notifier<T> : NotifierBase<T>
     {
-        List<IObserver<T>> observers = new List<IObserver<T>>();
-
-        public IDisposable Subscribe(IObserver<T> observer)
-        {
-            if (observer == null) throw new ArgumentNullException("observer");
-
-            observers.Add(observer);
-            return Disposable.FromAction(() => observers.Remove(observer));
-        }
-
         public void NotifyNext(T value)
         {
-            observers.ForEach(o => o.OnNext(value));
+            OnNext(value);
         }
 
         public void NotifyError(Exception error)
         {
-            observers.ForEach(o => o.OnError(error));
+            OnError(error);
         }
 
         public void NotifyCompleted()
         {
-            observers.ForEach(o => o.OnCompleted());
+            OnCompleted();
         }
     }
 }
---
+++ CoreLab/Core/Reactive/NotifierBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KLibrary.Labs.Reactive
+{
+    public abstract class NotifierBase<T> : IObservable<T>
+    {
+        List<IObserver<T>> observers = new List<IObserver<T>>();
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            if (observer == null) throw new ArgumentNullException("observer");
+
+            observers.Add(observer);
+            return Disposable.FromAction(() => observers.Remove(observer));
+        }
+
+        protected void OnNext(T value)
+        {
+            observers.ForEach(o => o.OnNext(value));
+        }
+
+        protected void OnError(Exception error)
+        {
+            observers.ForEach(o => o.OnError(error));
+        }
+
+        protected void OnCompleted()
+        {
+            observers.ForEach(o => o.OnCompleted());
+        }
+    }
+}
