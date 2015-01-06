26e50e5505606d60c9107fd9cd9f2b5272da4b91
Keiho Sakapon <git@saka-pon.net>
2015-01-07 01:44:27 +0900

    Add FrequencyMeter class

---
+++ CoreLab/Core/Mathematics/FrequencyMeter.cs
@@ -0,0 +1,37 @@
+﻿using KLibrary.Labs.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KLibrary.Labs.Mathematics
+{
+    public class FrequencyMeter
+    {
+        const int MaxCount = 100;
+        static readonly TimeSpan MaxSpan = TimeSpan.FromSeconds(1);
+
+        History<bool> history;
+
+        public double Frequency { get; private set; }
+
+        public double PeriodSeconds
+        {
+            get { return 1 / Frequency; }
+        }
+
+        public FrequencyMeter()
+        {
+            history = new History<bool>(MaxCount, MaxSpan);
+        }
+
+        public double Record()
+        {
+            history.Record(false);
+
+            if (history.Count < 2) return (Frequency = 0);
+
+            var frequency = (history.Count - 1) / (history.LastItem.Timestamp - history.FirstItem.Timestamp).TotalSeconds;
+            return (Frequency = frequency);
+        }
+    }
+}
