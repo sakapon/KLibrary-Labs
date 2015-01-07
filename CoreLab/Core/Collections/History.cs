3b1b16a2f31b9358889c39b39ac0d3d6f21ba0b2
Keiho Sakapon <github@saka-pon.net>
2015-01-07 15:31:00 +0900

    Add History class

---
+++ CoreLab/Core/Collections/History.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace KLibrary.Labs.Collections
+{
+    public class History<T> : Collection<HistoryItem<T>>
+    {
+        public int MaxCount { get; private set; }
+
+        public bool IsFull
+        {
+            get { return Count == MaxCount; }
+        }
+
+        public History(int maxCount)
+        {
+            if (maxCount <= 0) throw new ArgumentOutOfRangeException("maxCount", maxCount, "The value must be positive.");
+
+            MaxCount = maxCount;
+        }
+
+        public void Record(T value)
+        {
+            if (IsFull) RemoveAt(0);
+
+            Add(new HistoryItem<T>(value));
+        }
+    }
+
+    [DebuggerDisplay(@"\{{Timestamp}: {Value}\}")]
+    public struct HistoryItem<T>
+    {
+        public DateTime Timestamp { get; private set; }
+        public T Value { get; private set; }
+
+        public HistoryItem(T value)
+            : this()
+        {
+            Timestamp = DateTime.Now;
+            Value = value;
+        }
+    }
+}
---
+++ CoreLab/UnitTest/Collections/HistoryTest.cs
@@ -0,0 +1,61 @@
+﻿using KLibrary.Labs.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTest.Collections
+{
+    [TestClass]
+    public class HistoryTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ctor_0()
+        {
+            var history = new History<int>(0);
+        }
+
+        [TestMethod]
+        public void ctor_1()
+        {
+            var history = new History<int>(1);
+            Assert.AreEqual(1, history.MaxCount);
+            Assert.AreEqual(false, history.IsFull);
+
+            history.Record(123);
+            Assert.AreEqual(1, history.MaxCount);
+            Assert.AreEqual(true, history.IsFull);
+            Assert.AreEqual(123, history.First().Value);
+
+            history.Record(456);
+            Assert.AreEqual(1, history.MaxCount);
+            Assert.AreEqual(true, history.IsFull);
+            Assert.AreEqual(456, history.First().Value);
+        }
+
+        [TestMethod]
+        public void ctor_3()
+        {
+            var history = new History<int>(3);
+            Assert.AreEqual(3, history.MaxCount);
+            Assert.AreEqual(false, history.IsFull);
+
+            history.Record(123);
+            history.Record(234);
+            Assert.AreEqual(3, history.MaxCount);
+            Assert.AreEqual(false, history.IsFull);
+            Assert.AreEqual(234, history.Last().Value);
+
+            history.Record(345);
+            Assert.AreEqual(3, history.MaxCount);
+            Assert.AreEqual(true, history.IsFull);
+            Assert.AreEqual(345, history.Last().Value);
+
+            history.Record(456);
+            Assert.AreEqual(3, history.MaxCount);
+            Assert.AreEqual(true, history.IsFull);
+            Assert.AreEqual(456, history.Last().Value);
+        }
+    }
+}
