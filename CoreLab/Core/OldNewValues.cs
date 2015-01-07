c02f7ebdd893c9fab1529328f60f313a66f63338
Keiho Sakapon <github@saka-pon.net>
2015-01-07 16:06:40 +0900

    Add OldNewValues structure

---
+++ CoreLab/Core/OldNewValues.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KLibrary.Labs
+{
+    public struct OldNewValues<T>
+    {
+        public T OldValue { get; private set; }
+        public T NewValue { get; private set; }
+
+        public OldNewValues(T defaultValue)
+            : this()
+        {
+            OldValue = defaultValue;
+            NewValue = defaultValue;
+        }
+
+        public void UpdateValue(T newValue)
+        {
+            OldValue = NewValue;
+            NewValue = newValue;
+        }
+    }
+}
