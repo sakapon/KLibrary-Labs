029d6a61bf229702719958d896914db6c95a0877
Keiho Sakapon <github@saka-pon.net>
2015-01-08 17:01:07 +0900

    Add Observable2 class

---
+++ CoreLab/Core/Reactive/Observable2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KLibrary.Labs.Reactive
+{
+    public static class Observable2
+    {
+        public static IObservable<long> Interval(TimeSpan interval)
+        {
+            return new TimeTicker(interval);
+        }
+    }
+}
---
+++ CoreLab/UnitTest/Mathematics/FrequencyMeterTest.cs
@@ -0,0 +1,49 @@
+﻿using KLibrary.Labs.Mathematics;
+using KLibrary.Labs.Reactive;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace UnitTest.Mathematics
+{
+    [TestClass]
+    public class FrequencyMeterTest
+    {
+        [TestMethod]
+        public void ctor_1()
+        {
+            var meter = new FrequencyMeter();
+
+            Observable2.Interval(TimeSpan.FromMilliseconds(30))
+                .Select(_ => meter.Record())
+                .Subscribe(Console.WriteLine);
+
+            Thread.Sleep(3000);
+        }
+
+        [TestMethod]
+        public void ctor_2()
+        {
+            var meter = new FrequencyMeter();
+
+            Observable2.Interval(TimeSpan.FromMilliseconds(5))
+                .Select(_ => meter.Record())
+                .Subscribe(Console.WriteLine);
+
+            Thread.Sleep(3000);
+        }
+
+        [TestMethod]
+        public void ctor_100_5()
+        {
+            var meter = new FrequencyMeter(200, TimeSpan.FromSeconds(5));
+
+            Observable2.Interval(TimeSpan.FromMilliseconds(30))
+                .Select(_ => meter.Record())
+                .Subscribe(Console.WriteLine);
+
+            Thread.Sleep(3000);
+        }
+    }
+}
