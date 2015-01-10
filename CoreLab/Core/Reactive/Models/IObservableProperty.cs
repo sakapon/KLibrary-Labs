89874520f968cd175b2d7ceb4f60f89e74f41b8e
Keiho Sakapon <git@saka-pon.net>
2015-01-11 07:05:50 +0900

    Add ObservablePropertyBase class

---
+++ CoreLab/Core/Reactive/Models/IObservableProperty.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace KLibrary.Labs.Reactive.Models
+{
+    public interface IObservableProperty
+    {
+        IDisposable Subscribe(Action onValueChanged);
+    }
+}
---
+++ CoreLab/Core/Reactive/Models/ObservablePropertyBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace KLibrary.Labs.Reactive.Models
+{
+    public abstract class ObservablePropertyBaseCore<T> : NotifierBase<T>, IObservableProperty
+    {
+        public abstract T Value { get; set; }
+
+        public virtual void NotifyValueChanged()
+        {
+            OnNext(Value);
+        }
+
+        public IDisposable Subscribe(Action onValueChanged)
+        {
+            if (onValueChanged == null) throw new ArgumentNullException("onValueChanged");
+
+            return Subscribe(new ActionObserver<T>(o => onValueChanged()));
+        }
+    }
+
+    public abstract class ObservablePropertyBase<T> : ObservablePropertyBaseCore<T>, INotifyPropertyChanged
+    {
+        static readonly PropertyChangedEventArgs EventArgs_ValueChanged = new PropertyChangedEventArgs("Value");
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override void NotifyValueChanged()
+        {
+            base.NotifyValueChanged();
+
+            var pc = PropertyChanged;
+            if (pc != null) pc(this, EventArgs_ValueChanged);
+        }
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            var pc = PropertyChanged;
+            if (pc != null) pc(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
