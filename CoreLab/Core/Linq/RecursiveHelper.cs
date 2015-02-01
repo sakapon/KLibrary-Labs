c82908c96e37d0b6c09e02a08ca9a92799c7a27a
Keiho Sakapon <git@saka-pon.net>
2015-02-02 00:41:38 +0900

    Add RecursiveHelper class

---
+++ CoreLab/Core/Linq/RecursiveHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KLibrary.Labs.Linq
+{
+    public static class RecursiveHelper
+    {
+        public static IEnumerable<TSource> EnumerateRecursively<TSource>(this TSource initialValue, Func<TSource, TSource> getNextItem)
+        {
+            if (getNextItem == null) throw new ArgumentNullException("getNextItem");
+
+            var o = initialValue;
+            while (true)
+            {
+                yield return o;
+                o = getNextItem(o);
+            }
+        }
+
+        public static IEnumerable<TSource> EnumerateRecursively<TSource>(this TSource initialValue, Func<TSource, IEnumerable<TSource>> getNextItems)
+        {
+            if (getNextItems == null) throw new ArgumentNullException("getNextItems");
+
+            return EnumerateRecursively_Private(initialValue, getNextItems);
+        }
+
+        static IEnumerable<TSource> EnumerateRecursively_Private<TSource>(TSource initialValue, Func<TSource, IEnumerable<TSource>> getNextItems)
+        {
+            yield return initialValue;
+
+            foreach (var child in getNextItems(initialValue))
+            {
+                foreach (var o in EnumerateRecursively_Private(child, getNextItems))
+                {
+                    yield return o;
+                }
+            }
+        }
+    }
+}
---
+++ CoreLab/UnitTest/Linq/RecursiveHelperTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using KLibrary.Labs;
+using KLibrary.Labs.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Linq
+{
+    [TestClass]
+    public class RecursiveHelperTest
+    {
+        [TestMethod]
+        public void EnumerateRecursively_Seq()
+        {
+            var actual = 1
+                .EnumerateRecursively(i => i + 2)
+                .TakeWhile(i => i < 10)
+                .ToArray();
+            var expected = new[] { 1, 3, 5, 7, 9 };
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EnumerateRecursively_Parents()
+        {
+            var query = Environment.CurrentDirectory
+                .EnumerateRecursively(p =>
+                {
+                    var di = Directory.GetParent(p);
+                    return di != null ? di.FullName : null;
+                })
+                .TakeWhile(p => p != null);
+
+            foreach (var path in query)
+            {
+                Console.WriteLine(path);
+            }
+        }
+
+        [TestMethod]
+        public void EnumerateRecursively_Children()
+        {
+            var query = Path.GetFullPath(@"..\..\")
+                .EnumerateRecursively(p => Directory.EnumerateDirectories(p));
+
+            foreach (var path in query)
+            {
+                Console.WriteLine(path);
+            }
+        }
+    }
+}
