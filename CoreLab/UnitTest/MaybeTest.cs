2508cfb06c5444b59b7959ce9af6408dd42f2ee1
Keiho Sakapon <github@saka-pon.net>
2015-02-18 17:10:43 +0900

    Add test

--- CoreLab/UnitTest/MaybeTest.cs
+++ CoreLab/UnitTest/MaybeTest.cs
@@ -8,17 +8,6 @@ namespace UnitTest
     public class MaybeTest
     {
         [TestMethod]
-        public void Map()
-        {
-            var r1 = 2.ToMaybe().Select(i => 3 * i);
-            Assert.IsTrue(r1.HasValue);
-            Assert.AreEqual(6, (int)r1);
-
-            var r2 = Maybe<int>.None.Select(i => 3 * i);
-            Assert.IsFalse(r2.HasValue);
-        }
-
-        [TestMethod]
         public void Equals()
         {
             Assert.IsTrue(Maybe<int>.None == default(Maybe<int>));
@@ -35,6 +24,48 @@ namespace UnitTest
             Assert.IsFalse(Maybe<object>.None == obj);
             Assert.IsFalse(obj == Maybe<object>.None);
             Assert.IsFalse(obj.ToMaybe() == new object());
+        }
+
+        [TestMethod]
+        public void Do()
+        {
+            var flag = 0;
+            Action<int> action = i => flag = i;
+
+            var m1 = 1.ToMaybe().Where(i => i % 2 == 0).Do(action);
+            Assert.AreEqual(0, flag);
+            Assert.AreEqual(0, m1.Value);
+
+            var m2 = 2.ToMaybe().Where(i => i % 2 == 0).Do(action);
+            Assert.AreEqual(2, flag);
+            Assert.AreEqual(2, m2.Value);
+        }
+
+        [TestMethod]
+        public void Map()
+        {
+            var r1 = 2.ToMaybe().Select(i => 3 * i);
+            Assert.IsTrue(r1.HasValue);
+            Assert.AreEqual(6, (int)r1);
+
+            var r2 = Maybe<int>.None.Select(i => 3 * i);
+            Assert.IsFalse(r2.HasValue);
+        }
+
+        [TestMethod]
+        public void Combine()
+        {
+            Func<int, string, string> format = (i, f) => string.Format(f, i);
+
+            var m1 = 1.ToMaybe().Combine("int:{0}", format);
+            Assert.AreEqual(true, m1.HasValue);
+            Assert.AreEqual("int:1", m1.Value);
+            var m2 = 1.ToMaybe().Combine(null, format);
+            Assert.AreEqual(false, m2.HasValue);
+            Assert.AreEqual(null, m2.Value);
+            var m3 = Maybe<int>.None.Combine("int:{0}", format);
+            Assert.AreEqual(false, m3.HasValue);
+            Assert.AreEqual(null, m3.Value);
         }
 
         [TestMethod]
