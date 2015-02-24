49150baba698b5332c9085036c881a33961d9989
Keiho Sakapon <github@saka-pon.net>
2015-02-24 18:35:00 +0900

    Add ScaleConverter class

---
+++ UILab/UI/UI/Converters/ScaleConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KLibrary.Labs.UI.Converters
+{
+    // TODO: Add Scale property.
+    [ValueConversion(typeof(double), typeof(double))]
+    public class ScaleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valueD = ToValue(value);
+            var scale = ToScale(parameter);
+
+            return valueD.HasValue ? valueD * scale : DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valueD = ToValue(value);
+            var scale = ToScale(parameter);
+
+            return valueD.HasValue && scale != 0.0 ? valueD / scale : DependencyProperty.UnsetValue;
+        }
+
+        static double? ToValue(object o)
+        {
+            try
+            {
+                return System.Convert.ToDouble(o);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        static double ToScale(object o)
+        {
+            try
+            {
+                return o == null ? 1.0 : System.Convert.ToDouble(o);
+            }
+            catch (Exception)
+            {
+                return 1.0;
+            }
+        }
+    }
+}
