e4155f97e07d6c9bc539ff0ede872a047d1f454a
Keiho Sakapon <github@saka-pon.net>
2015-03-04 13:56:04 +0900

    Add converters

---
+++ UILab/UI/UI/Converters/SolidColorBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace KLibrary.Labs.UI.Converters
+{
+    [ValueConversion(typeof(Color), typeof(SolidColorBrush))]
+    public class SolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Color
+                ? new SolidColorBrush((Color)value)
+                : DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is SolidColorBrush
+                ? ((SolidColorBrush)value).Color
+                : DependencyProperty.UnsetValue;
+        }
+    }
+}
