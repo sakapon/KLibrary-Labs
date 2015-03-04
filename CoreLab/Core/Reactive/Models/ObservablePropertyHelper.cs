c4ec16fb1431e3acc583a3fa70723a965866d467
Keiho Sakapon <github@saka-pon.net>
2015-03-04 19:19:02 +0900

    Add overloads of ToGetProperty method #6

--- CoreLab/Core/Reactive/Models/ObservablePropertyHelper.cs
+++ CoreLab/Core/Reactive/Models/ObservablePropertyHelper.cs
@@ -74,6 +74,12 @@ namespace KLibrary.Labs.Reactive.Models
             return new ObservableEvent<TSource>();
         }
 
+        /// <summary>
+        /// Creates a wrapper of an <see cref="IObservable&lt;TSource&gt;"/> object to restrict members to be accessed.
+        /// </summary>
+        /// <typeparam name="TSource">The type of objects to be provided.</typeparam>
+        /// <param name="source">An <see cref="IObservable&lt;TSource&gt;"/> object.</param>
+        /// <returns>A wrapped <see cref="IObservable&lt;TSource&gt;"/> object.</returns>
         public static IObservable<TSource> ToObservableMask<TSource>(this IObservable<TSource> source)
         {
             if (source == null) throw new ArgumentNullException("source");
@@ -81,6 +87,12 @@ namespace KLibrary.Labs.Reactive.Models
             return new ObservableMask<TSource>(source);
         }
 
+        /// <summary>
+        /// Creates a wrapper of an <see cref="IObservableProperty&lt;TSource&gt;"/> object to restrict members to be accessed.
+        /// </summary>
+        /// <typeparam name="TSource">The type of objects to be provided.</typeparam>
+        /// <param name="source">An <see cref="IObservableProperty&lt;TSource&gt;"/> object.</param>
+        /// <returns>A wrapped <see cref="IObservableGetProperty&lt;TSource&gt;"/> object.</returns>
         public static IObservableGetProperty<TSource> ToGetPropertyMask<TSource>(this IObservableProperty<TSource> source)
         {
             if (source == null) throw new ArgumentNullException("source");
@@ -88,6 +100,12 @@ namespace KLibrary.Labs.Reactive.Models
             return new ObservableGetPropertyMask<TSource>(source);
         }
 
+        /// <summary>
+        /// Creates an <see cref="IObservableGetProperty&lt;TSource&gt;"/> from an <see cref="IObservable&lt;TSource&gt;"/>, with the default value of <typeparamref name="TSource"/>.
+        /// </summary>
+        /// <typeparam name="TSource">The type of objects to be provided.</typeparam>
+        /// <param name="source">An <see cref="IObservable&lt;TSource&gt;"/> object.</param>
+        /// <returns>An <see cref="IObservableGetProperty&lt;TSource&gt;"/> object.</returns>
         public static IObservableGetProperty<TSource> ToGetProperty<TSource>(this IObservable<TSource> source)
         {
             if (source == null) throw new ArgumentNullException("source");
@@ -95,11 +113,52 @@ namespace KLibrary.Labs.Reactive.Models
             return new FollowingGetProperty<TSource>(source);
         }
 
+        /// <summary>
+        /// Creates an <see cref="IObservableGetProperty&lt;TSource&gt;"/> from an <see cref="IObservable&lt;TSource&gt;"/>, with the specified default value.
+        /// </summary>
+        /// <typeparam name="TSource">The type of objects to be provided.</typeparam>
+        /// <param name="source">An <see cref="IObservable&lt;TSource&gt;"/> object.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>An <see cref="IObservableGetProperty&lt;TSource&gt;"/> object.</returns>
         public static IObservableGetProperty<TSource> ToGetProperty<TSource>(this IObservable<TSource> source, TSource defaultValue)
         {
             if (source == null) throw new ArgumentNullException("source");
 
             return new FollowingGetProperty<TSource>(source, defaultValue);
+        }
+
+        /// <summary>
+        /// Creates an <see cref="IObservableGetProperty&lt;TResult&gt;"/> from another <see cref="IObservableProperty&lt;TSource&gt;"/>.
+        /// The default value is determined by the value of <paramref name="source"/>.
+        /// </summary>
+        /// <typeparam name="TSource">The type of objects to be provided.</typeparam>
+        /// <typeparam name="TResult">The type of mapped objects.</typeparam>
+        /// <param name="source">An <see cref="IObservableProperty&lt;TSource&gt;"/> object.</param>
+        /// <param name="mapping">A transform function to apply to each element.</param>
+        /// <returns>An <see cref="IObservableGetProperty&lt;TResult&gt;"/> object.</returns>
+        public static IObservableGetProperty<TResult> ToGetProperty<TSource, TResult>(this IObservableProperty<TSource> source, Func<TSource, TResult> mapping)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (mapping == null) throw new ArgumentNullException("mapping");
+
+            return new FollowingGetProperty<TResult>(source.Map(mapping), mapping(source.Value));
+        }
+
+        /// <summary>
+        /// Creates an <see cref="IObservableGetProperty&lt;TResult&gt;"/> from another <see cref="IObservableGetProperty&lt;TSource&gt;"/>.
+        /// The default value is determined by the value of <paramref name="source"/>.
+        /// </summary>
+        /// <typeparam name="TSource">The type of objects to be provided.</typeparam>
+        /// <typeparam name="TResult">The type of mapped objects.</typeparam>
+        /// <param name="source">An <see cref="IObservableGetProperty&lt;TSource&gt;"/> object.</param>
+        /// <param name="mapping">A transform function to apply to each element.</param>
+        /// <returns>An <see cref="IObservableGetProperty&lt;TResult&gt;"/> object.</returns>
+        public static IObservableGetProperty<TResult> ToGetProperty<TSource, TResult>(this IObservableGetProperty<TSource> source, Func<TSource, TResult> mapping)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (mapping == null) throw new ArgumentNullException("mapping");
+
+            return new FollowingGetProperty<TResult>(source.Map(mapping), mapping(source.Value));
         }
 
         public static IObservable<TSource> Do<TSource, TProperty>(this IObservable<TSource> source, IObservableGetProperty<TProperty> property)
