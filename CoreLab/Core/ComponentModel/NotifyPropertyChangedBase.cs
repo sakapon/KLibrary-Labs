b4a42335627f7fab8b6418d52878217c99634069
Keiho Sakapon <github@saka-pon.net>
2015-03-06 19:30:14 +0900

    Add NotifyPropertyChangedBase class

---
+++ CoreLab/Core/ComponentModel/NotifyPropertyChangedBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace KLibrary.Labs.ComponentModel
+{
+    public abstract class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            var pc = PropertyChanged;
+            if (pc != null) pc(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
