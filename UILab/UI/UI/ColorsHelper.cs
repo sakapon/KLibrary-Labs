c15104d5203e544ff39a5a22dcd9a626be18ae60
Keiho Sakapon <git@saka-pon.net>
2015-03-10 23:59:55 +0900

    Improve code

--- UILab/UI/UI/ColorsHelper.cs
+++ UILab/UI/UI/ColorsHelper.cs
@@ -3,31 +3,41 @@ using System.Windows.Media;
 
 namespace KLibrary.Labs.UI
 {
+    /// <summary>
+    /// Provides the helper methods for colors.
+    /// </summary>
     public static class ColorsHelper
     {
-        // 数値をヒートマップ用の色に変換します。
-        // 数値の範囲は、0 以上 maxValue 以下です。
-        // 色は、0000FF - 00FFFF- 00FF00 - FFFF00 - FF0000 の間の連続値で表されます。
-        public static Color ToHeatColor(double value, double maxValue)
+        /// <summary>
+        /// Converts a value over [0, 1) to a color for heat maps.
+        /// </summary>
+        /// <param name="value">A value over [0, 1).</param>
+        /// <returns>A color for heat maps (between blue and red). Blue if <paramref name="value"/> &lt; 0; red if <paramref name="value"/> &gt;= 1.</returns>
+        /// <remarks>
+        /// Color scale: 0000FF - 00FFFF - 00FF00 - FFFF00 - FF0000.
+        /// </remarks>
+        public static Color ToHeatColor(double value)
         {
             if (value < 0) return Color.FromRgb(0, 0, 255);
-            if (value > maxValue) return Color.FromRgb(255, 0, 0);
+            if (value >= 1) return Color.FromRgb(255, 0, 0);
 
-            var red =
-                value < maxValue / 2 ? 0 :
-                value < 3 * maxValue / 4 ? 255 * 4 * (value - maxValue / 2) / maxValue :
-                255;
-            var green =
-                value < maxValue / 4 ? 255 * 4 * value / maxValue :
-                value < 3 * maxValue / 4 ? 255 :
-                255 * 4 * (maxValue - value) / maxValue;
-            var blue =
-                value < maxValue / 4 ? 255 :
-                value < maxValue / 2 ? 255 * 4 * (maxValue / 2 - value) / maxValue :
-                0;
-            return Color.FromRgb(RoundToByte(red), RoundToByte(green), RoundToByte(blue));
+            // [0, 256) の範囲の double 値を扱い、最後に (byte)v で変換します。
+            var red
+                = value < 0.5 ? 0
+                : value < 0.75 ? 256 * 4 * (value - 0.5)
+                : 256;
+            var green
+                = value < 0.25 ? 256 * 4 * value
+                : value < 0.75 ? 256
+                : 256 * 4 * (1 - value);
+            var blue
+                = value < 0.25 ? 256
+                : value < 0.5 ? 256 * 4 * (0.5 - value)
+                : 0;
+
+            return Color.FromRgb(ToByte(red), ToByte(green), ToByte(blue));
         }
 
-        static readonly Func<double, byte> RoundToByte = v => (byte)Math.Round(v, MidpointRounding.AwayFromZero);
+        static readonly Func<double, byte> ToByte = v => (byte)(v == 256.0 ? 255.0 : v);
     }
 }
---
+++ UILab/UnitTest/UI/ColorsHelperTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Media;
+using KLibrary.Labs.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.UI
+{
+    [TestClass]
+    public class ColorsHelperTest
+    {
+        [TestMethod]
+        public void ToHeatColor()
+        {
+            Assert.AreEqual(Colors.Blue, ColorsHelper.ToHeatColor(0.0));
+            Assert.AreEqual(Colors.Cyan, ColorsHelper.ToHeatColor(0.25));
+            Assert.AreEqual(Color.FromRgb(0, 255, 0), ColorsHelper.ToHeatColor(0.5));
+            Assert.AreEqual(Colors.Yellow, ColorsHelper.ToHeatColor(0.75));
+            Assert.AreEqual(Colors.Red, ColorsHelper.ToHeatColor(1.0));
+        }
+    }
+}
