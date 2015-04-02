3b0056679a8d0391d039e57adbc1afdbe8f68034
Keiho Sakapon <github@saka-pon.net>
2015-04-02 18:29:11 +0900

    Add comments

--- ObservableModelLab/ObservableModel/ObservableModel/ObservableProperty.cs
+++ ObservableModelLab/ObservableModel/ObservableModel/ObservableProperty.cs
@@ -84,7 +84,7 @@ namespace KLibrary.Labs.ObservableModel
         /// <summary>
         /// Creates an instance of IObservable-based get-only property from the predecessor settable property.
         /// </summary>
-        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TSource">The type of the source property.</typeparam>
         /// <typeparam name="TResult">The type of the property.</typeparam>
         /// <param name="source">The source property.</param>
         /// <param name="selector">The transform function.</param>
@@ -105,7 +105,7 @@ namespace KLibrary.Labs.ObservableModel
         /// <summary>
         /// Creates an instance of IObservable-based get-only property from the predecessor get-only property.
         /// </summary>
-        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TSource">The type of the source property.</typeparam>
         /// <typeparam name="TResult">The type of the property.</typeparam>
         /// <param name="source">The source property.</param>
         /// <param name="selector">The transform function.</param>
@@ -123,6 +123,13 @@ namespace KLibrary.Labs.ObservableModel
             return new FollowingGetOnlyProperty<TResult>(source.Select(selector), selector(source.Value), notifiesUnchanged);
         }
 
+        /// <summary>
+        /// Creates a mask to capsulate the source sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <param name="source">The source sequence of elements.</param>
+        /// <returns>An <see cref="IObservable&lt;TSource&gt;"/> object.</returns>
+        [Obsolete("Use AsObservable method.")]
         public static IObservable<TSource> ToObservableMask<TSource>(this IObservable<TSource> source)
         {
             if (source == null) throw new ArgumentNullException("source");
@@ -130,6 +137,12 @@ namespace KLibrary.Labs.ObservableModel
             return new ObservableMask<TSource>(source);
         }
 
+        /// <summary>
+        /// Creates a mask to capsulate the source settable property.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the property.</typeparam>
+        /// <param name="source">The source property.</param>
+        /// <returns>An <see cref="IGetOnlyProperty&lt;TSource&gt;"/> object.</returns>
         public static IGetOnlyProperty<TSource> ToGetOnlyMask<TSource>(this ISettableProperty<TSource> source)
         {
             if (source == null) throw new ArgumentNullException("source");
@@ -137,6 +150,14 @@ namespace KLibrary.Labs.ObservableModel
             return new GetOnlyPropertyMask<TSource>(source);
         }
 
+        /// <summary>
+        /// Notifies the property to update the value, for each element.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
+        /// <param name="source">The source sequence of elements.</param>
+        /// <param name="property">The get-only property.</param>
+        /// <returns>A new source sequence.</returns>
         public static IObservable<TSource> Do<TSource, TProperty>(this IObservable<TSource> source, IGetOnlyProperty<TProperty> property)
         {
             if (source == null) throw new ArgumentNullException("source");
@@ -145,6 +166,14 @@ namespace KLibrary.Labs.ObservableModel
             return source.Do(o => property.OnNext());
         }
 
+        /// <summary>
+        /// Notifies the property to update the value, for each element.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the source.</typeparam>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
+        /// <param name="source">The source sequence of elements.</param>
+        /// <param name="property">The get-only property.</param>
+        /// <returns>An <see cref="IDisposable"/> object used to unsubscribe from the source sequence.</returns>
         public static IDisposable Subscribe<TSource, TProperty>(this IObservable<TSource> source, IGetOnlyProperty<TProperty> property)
         {
             if (source == null) throw new ArgumentNullException("source");
