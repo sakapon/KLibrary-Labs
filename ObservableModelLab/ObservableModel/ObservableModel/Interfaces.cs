d36659786ac4f4d61bcbb28c5c449b922e8b024b
Keiho Sakapon <github@saka-pon.net>
2015-04-02 14:08:36 +0900

    Add interfaces

---
+++ ObservableModelLab/ObservableModel/ObservableModel/Interfaces.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace KLibrary.Labs.ObservableModel
+{
+    public interface IObservable2<out T> : IObservable<T>
+    {
+        bool HasObservers { get; }
+    }
+
+    public interface IJunction<T> : IObservable2<T>, IObserver<T>
+    {
+    }
+
+    public interface ISettableProperty<T> : IJunction<T>, INotifyPropertyChanged
+    {
+        T Value { get; set; }
+        bool NotifiesUnchanged { get; }
+    }
+
+    public interface IGetOnlyProperty<out T> : IObservable2<T>, INotifyPropertyChanged
+    {
+        T Value { get; }
+        bool NotifiesUnchanged { get; }
+        void OnNext();
+    }
+}
