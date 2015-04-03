7247eeee349701dad8d18d91aaacb77d736f26e2
Keiho Sakapon <github@saka-pon.net>
2015-04-03 18:16:36 +0900

    Add sample from KLibrary.ComponentModel

---
+++ Samples/WpfSample1/BmiWpf/Body.cs
@@ -0,0 +1,30 @@
+﻿using KLibrary.ComponentModel;
+using System;
+using System.ComponentModel;
+
+namespace BmiWpf
+{
+    public class Body : NotifyBase
+    {
+        [DefaultValue(170.0)]
+        public double Height
+        {
+            get { return GetValue<double>(); }
+            set { SetValue(value); }
+        }
+
+        [DefaultValue(70.0)]
+        public double Weight
+        {
+            get { return GetValue<double>(); }
+            set { SetValue(value); }
+        }
+
+        [DependentOn("Height")]
+        [DependentOn("Weight")]
+        public double Bmi
+        {
+            get { return Weight / Math.Pow(Height / 100, 2); }
+        }
+    }
+}
