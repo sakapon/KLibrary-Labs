2cb8666109f7219e66db10d2540a05ec7fe3ab5f
Keiho Sakapon <git@saka-pon.net>
2015-04-05 23:45:30 +0900

    Add MainViewModel class

---
+++ Samples/WpfSample1/BmiWpf2/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using KLibrary.Labs.ObservableModel;
+
+namespace BmiWpf2
+{
+    public class MainViewModel
+    {
+        public Body Body { get; private set; }
+
+        public IGetOnlyProperty<double> BmiRectangleWidth { get; private set; }
+        public IGetOnlyProperty<string> BmiRectangleFill { get; private set; }
+
+        public MainViewModel()
+        {
+            Body = new Body();
+
+            BmiRectangleWidth = Body.Bmi.SelectToGetOnly(i => 5 * i);
+            BmiRectangleFill = Body.Bmi.SelectToGetOnly(i => i < 25 ? "#FF009900" : i < 40 ? "#FFDDDD00" : "#FFEE0000");
+        }
+    }
+}
