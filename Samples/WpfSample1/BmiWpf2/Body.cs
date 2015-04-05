640618fec04894940bb124e602bdb99ad66f0156
Keiho Sakapon <git@saka-pon.net>
2015-04-05 23:31:38 +0900

    Add Body class

---
+++ Samples/WpfSample1/BmiWpf2/Body.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Linq;
+using KLibrary.Labs.ObservableModel;
+
+namespace BmiWpf2
+{
+    public class Body
+    {
+        public ISettableProperty<double> Height { get; private set; }
+        public ISettableProperty<double> Weight { get; private set; }
+        public IGetOnlyProperty<double> Bmi { get; private set; }
+
+        public Body()
+        {
+            Height = ObservableProperty.CreateSettable(170.0);
+            Weight = ObservableProperty.CreateSettable(70.0);
+            Bmi = ObservableProperty.CreateGetOnly(() => Weight.Value / Math.Pow(Height.Value / 100, 2));
+
+            Height.Merge(Weight).Subscribe(Bmi);
+        }
+    }
+}
