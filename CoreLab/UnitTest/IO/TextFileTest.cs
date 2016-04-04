e50c643d84090ea8319242087e77d9b23c21a028
Keiho Sakapon <saka_pon@live.jp>
2016-04-04 15:40:54 +0900

    Add test

---
+++ CoreLab/UnitTest/IO/TextFileTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using KLibrary.Labs.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.IO
+{
+    [TestClass]
+    public class TextFileTest
+    {
+        static readonly string[] Lines = new[] { "123", "あいう" };
+        const string Content1 = @"123
+あいう
+";
+        const string Content2 = @"123
+あいう";
+
+        [TestMethod]
+        public void ReadLines_1()
+        {
+            using (var stream = new MemoryStream(TextFile.UTF8N.GetBytes(Content1)))
+            {
+                var records_actual = stream.ReadLines().ToArray();
+
+                for (var i = 0; i < Lines.Length; i++)
+                    Assert.AreEqual(Lines[i], records_actual[i]);
+            }
+        }
+
+        [TestMethod]
+        public void WriteLines_1()
+        {
+            using (var stream = new MemoryStream())
+            {
+                stream.WriteLines(Lines);
+
+                CollectionAssert.AreEqual(TextFile.UTF8N.GetBytes(Content1), stream.ToArray());
+            }
+        }
+    }
+}
