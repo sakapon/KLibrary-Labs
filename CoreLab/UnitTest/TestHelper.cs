9820131f46aa0893a2fe6c2e85b3399f6343a6aa
Keiho Sakapon <saka_pon@live.jp>
2016-04-08 15:10:12 +0900

    Add classes for test

---
+++ CoreLab/UnitTest/TestHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    public static class TestHelper
+    {
+        [DebuggerStepThrough]
+        public static void AreCollectionEqual(ICollection<System.Collections.ICollection> expected, ICollection<System.Collections.ICollection> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            foreach (var _ in expected.Zip(actual, (e, a) => new { e, a }))
+                CollectionAssert.AreEqual(_.e, _.a);
+        }
+    }
+}
