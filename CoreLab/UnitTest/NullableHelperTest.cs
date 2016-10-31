d7db5bb5572706fd83fc2de4472fe71b6932d5a8
Keiho Sakapon <git@saka-pon.net>
2016-10-31 16:03:13 +0900

    Add test

--- CoreLab/UnitTest/NullableHelperTest.cs
+++ CoreLab/UnitTest/NullableHelperTest.cs
@@ -30,5 +30,41 @@ namespace UnitTest
             default(int?).IfNotNull(action);
             Assert.AreEqual(1, i);
         }
+
+        [TestMethod]
+        public void IfNotNull_Func_Class()
+        {
+            Func<string, string> func = s => s + s;
+
+            Assert.AreEqual("aa", "a".IfNotNull(func));
+            Assert.AreEqual(null, default(string).IfNotNull(func));
+        }
+
+        [TestMethod]
+        public void IfNotNull_Func_Struct()
+        {
+            Func<int, string> func = i => i.ToString();
+
+            Assert.AreEqual("1", ((int?)1).IfNotNull(func));
+            Assert.AreEqual(null, default(int?).IfNotNull(func));
+        }
+
+        [TestMethod]
+        public void IfNotNull2_Func_Class()
+        {
+            Func<string, int> func = s => int.Parse(s);
+
+            Assert.AreEqual(1, "1".IfNotNull2(func));
+            Assert.AreEqual(null, default(string).IfNotNull2(func));
+        }
+
+        [TestMethod]
+        public void IfNotNull2_Func_Struct()
+        {
+            Func<int, int> func = i => 2 * i;
+
+            Assert.AreEqual(6, ((int?)3).IfNotNull2(func));
+            Assert.AreEqual(null, default(int?).IfNotNull2(func));
+        }
     }
 }
